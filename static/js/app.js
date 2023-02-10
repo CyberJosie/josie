1655e595b5a34742d6cefe83dbf183efae670c69
CyberJosie <jocelyn@techjosie.com>
2023-02-10 16:41:19 -0700

    Add menu bar and dropdown are for complete menu

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+
+function toggle_menu() {
+    let menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
