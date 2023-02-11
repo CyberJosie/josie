c82e0966537386205645eea39a81c72769cc50d8
CyberJosie <jocelyn@techjosie.com>
2023-02-11 10:06:32 -0700

    Too much 2 remember, sorry i was high

--- Site/views.py
+++ Site/views.py
@@ -13,42 +13,48 @@ def set_timezone(request):
 
 def index(request):
     ctx = {}
-    ctx['page_title'] = 'Home | {}'.format(SITE_NAME)
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
+    ctx['page_heading'] = 'Home'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/index.html', context=ctx)
 
 def services(request):
     ctx = {}
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
-    ctx['page_title'] = 'Services | {}'.format(SITE_NAME)
+    ctx['page_heading'] = 'Services'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/services.html', context=ctx)
 
 def projects(request):
     ctx = {}
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
-    ctx['page_title'] = 'Projects | {}'.format(SITE_NAME)
+    ctx['page_heading'] = 'Projects'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/projects.html', context=ctx)
 
 def about_me(request):
     ctx = {}
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
-    ctx['page_title'] = 'About Me | {}'.format(SITE_NAME)
+        ctx['page_heading'] = 'About Me'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/about_me.html', context=ctx)
 
 def contact(request):
     ctx = {}
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
-    ctx['page_title'] = 'Contact | {}'.format(SITE_NAME)
+    ctx['page_heading'] = 'Contact'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/contact.html', context=ctx)
 
 def portfolio(request):
     ctx = {}
     if 'django_timezone' in request.session:
         ctx['date'], ctx['time'] = make_datetime(request.session['django_timezone'])
-    ctx['page_title'] = 'Portfolio | {}'.format(SITE_NAME)
+    ctx['page_heading'] = 'Portfolio'
+    ctx['page_title'] = '{} | {}'.format(ctx['page_heading'], SITE_NAME)
     return render(request, 'Site/portfolio.html', context=ctx)
